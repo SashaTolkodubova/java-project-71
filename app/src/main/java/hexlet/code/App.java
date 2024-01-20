a7eaa99873f6ed2f574b728e712a67b7cacf3d90
SashaTolkodubova <konstantin@Konstantins-Mac-mini.local>
2024-01-20 16:11:20 +0300

    add code on step3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,32 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.util.SortedMap;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 0.1",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Runnable {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new CommandLine(new App()).execute("-h");
+    }
+
+    @Option(names = {"-h", "--Help"}
+            , usageHelp = true
+            , description = "Show this help message and exit.")
+    boolean help;
+
+    @Option(names = {"-V", "--version"}
+            , versionHelp = true
+            , description = "Print version information and exit.")
+    boolean versionHelp;
+
+    @Override
+    public void run() {
+        System.out.println("Hello");
     }
 }
