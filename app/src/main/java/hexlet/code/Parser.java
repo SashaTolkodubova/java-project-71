5f18072a2208280c83f4760944a161060f0caf1d
SashaTolkodubova <konstantin@Konstantins-Mac-mini.local>
2024-02-01 17:39:30 +0300

    add code on step 7

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -1,14 +1,33 @@
 package hexlet.code;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Parser {
     public HashMap getMap(String filePath) throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
+        ObjectMapper mapperJSON = new JsonMapper();
+        ObjectMapper mapperYML = new YAMLMapper();
         File file = new File(filePath);
-        return mapper.readValue(file, HashMap.class);
+        final String sampleJSON = "(json$)";
+        final String sampleYML = "(yml$)";
+        Pattern patternJSON = Pattern.compile(sampleJSON);
+        Matcher matcherJSON = patternJSON.matcher(filePath);
+
+        Pattern patternYML = Pattern.compile(sampleYML);
+        Matcher matcherYML = patternYML.matcher(filePath);
+
+        if (matcherJSON.find()) {
+            return mapperJSON.readValue(file, HashMap.class);
+        } else if (matcherYML.find()) {
+            return mapperYML.readValue(file, HashMap.class);
+        } else {
+            throw new Exception("Format not supported");
+        }
     }
 }
--- app/src/test/java/hexlet/code/MainTest.java
+++ app/src/test/java/hexlet/code/MainTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 public class MainTest {
     @Test
-    public void testDiffer() throws Exception {
+    public void testDifferJSON() throws Exception {
         String expected = "{\n"
                 + "- follow: false\n"
                 + "  host: hexlet.io\n"
@@ -17,6 +17,22 @@ public class MainTest {
                 + "}";
         String path1 = "src/test/resources/file1.json";
         String path2 = "src/test/resources/file2.json";
+        String actual = Differ.generate(path1, path2);
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testDifferYML() throws Exception {
+        String expected = "{\n"
+                + "- follow: false\n"
+                + "  host: hexlet.io\n"
+                + "- proxy: 123.234.53.22\n"
+                + "- timeout: 50\n"
+                + "+ timeout: 20\n"
+                + "+ verbose: true\n"
+                + "}";
+        String path1 = "src/test/resources/file1.yml";
+        String path2 = "src/test/resources/file2.yml";
         String actual = Differ.generate(path1, path2);
         Assertions.assertEquals(actual, expected);
     }
