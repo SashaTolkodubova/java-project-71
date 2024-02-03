2219a4fe1d74768def17982a9741a9822f034716
SashaTolkodubova <konstantin@Konstantins-Mac-mini.local>
2024-02-03 14:45:51 +0300

    add code on finalTest7

--- app/src/main/java/hexlet/code/formatters/JSON.java
+++ app/src/main/java/hexlet/code/formatters/JSON.java
@@ -2,14 +2,20 @@ package hexlet.code.formatters;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-//import java.io.File;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.LinkedHashMap;
 
 public class JSON {
     public static String getResult(LinkedHashMap<String, Object> hashMap) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-//        objectMapper.writeValue(new File("app/src/main/resources/result.json"), hashMap);
-        return objectMapper.writeValueAsString(hashMap);
+        String result = objectMapper.writeValueAsString(hashMap);
+        BufferedWriter writer = new BufferedWriter(new FileWriter("app/src/main/resources/result.json"));
+        writer.write(result);
+        writer.close();
+
+        return result;
     }
 }
